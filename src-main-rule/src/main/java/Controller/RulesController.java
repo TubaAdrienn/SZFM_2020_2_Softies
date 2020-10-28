5c071f9fefde565f52ebec0433cafa6510fba199
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-10-28 17:50:25 +0100

    rule controller methods filled

--- src-main-rule/src/main/java/Controller/RulesController.java
+++ src-main-rule/src/main/java/Controller/RulesController.java
@@ -14,19 +14,38 @@ public class RulesController extends Controller {
     protected String rules;
 
     public RulesController(String game) {
-        //Constructor
+        this.game=game;
+        switch (game) {
+            case "tictactoe":
+                this.rules = "TicTacToe rules.";
+                break;
+            case "mastermind":
+                this.rules = "Mastermind rules.";
+                break;
+            case "2048":
+                this.rules = "2048 rules.";
+                break;
+            case "snake":
+                this.rules = "Snake rules.";
+                break;
+        }
     }
 
     @FXML
     private void initialize(){
-       //Init code
+        System.out.println("InitData");
+        rulesLabel.setText(rules);
     }
 
-    public void playClick (MouseEvent mouseEvent) {
-        // Pageloader loads page
+    public void playClick (MouseEvent mouseEvent) throws IOException {
+        try{
+            PageLoader.loadGame(mouseEvent, this.game, name1, name2);
+        } catch (Exception e){
+            System.out.println("Game not implemented yet.");
+        }
     }
 
-    public void backClick(MouseEvent mouseEvent) {
-        //Pageloader loads Main page
+    public void backClick(MouseEvent mouseEvent) throws IOException {
+        PageLoader.loadGame(mouseEvent, "main", name1, name2);
     }
 }
