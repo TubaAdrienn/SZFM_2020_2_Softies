60f63131e6123d5f32ff822503c60967d3f3c31d
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-10-28 17:49:48 +0100

    pageloader created

--- src-main-rule/src/main/java/Controller/PageLoader.java
+++ src-main-rule/src/main/java/Controller/PageLoader.java
@@ -1,17 +1,32 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class PageLoader {
 
     public static void loadGame(MouseEvent mouseEvent, String game, String name1, String name2) throws IOException {
-      //Load game page
+        System.out.println("/" + game + ".fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(PageLoader.class.getResource("/" + game + ".fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
-    public static void loadRules(MouseEvent mouseEvent, String game) throws IOException{
-        //Load Rules page
+    public static void loadRules(MouseEvent mouseEvent, String game) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PageLoader.class.getResource("/rules.fxml"));
+        RulesController controller = new RulesController(game);
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
