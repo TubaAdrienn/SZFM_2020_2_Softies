c977c23664a01ffb97f336c4f8fcf82b31645895
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-10-31 15:18:11 +0100

    Add pins to Mastermind

--- src/main/java/mastermind/Mastermind.java
+++ src/main/java/mastermind/Mastermind.java
@@ -1,8 +1,12 @@
 package mastermind;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Mastermind {
@@ -22,5 +26,43 @@ public class Mastermind {
 
         for (int i = 0; i < 4; i++)
             this.guessColors[i] = this.rand.nextInt(8);
+    }
+
+    public PinStruct process(GridPane leftPane, int lastStep) {
+        int whitePin = 0;
+        int blackPin = 0;
+        List<Integer> whiteExt = new ArrayList<Integer>();
+        List<Integer> whiteMyExt = new ArrayList<Integer>();
+
+        for (int i = 0; i < 4; i++) {
+            ImageView view = (ImageView) leftPane.getChildren().get(((int) Math.floor(lastStep / 4) - 1) * 4 + i);
+            Color color = Color.findByImage(view.getImage());
+            if (color != null)
+                if (color.equals(Color.getByValue(guessColors[i]))) {
+                    blackPin++;
+                    whiteExt.add(i);
+                    whiteMyExt.add(i);
+                }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            ImageView view = (ImageView) leftPane.getChildren().get(((int) Math.floor(lastStep / 4) - 1) * 4 + i);
+            Color color = Color.findByImage(view.getImage());
+            if (color != null)
+                if (!color.equals(Color.getByValue(guessColors[i])) && !whiteMyExt.contains(i)) {
+                    for (int j = 0; j < 4; j++) {
+                        Color guess = Color.getByValue(guessColors[j]);
+                        if (!whiteExt.contains(j) && !whiteMyExt.contains(i)) {
+                            if (color.equals(guess)) {
+                                whiteExt.add(j);
+                                whiteMyExt.add(i);
+                                whitePin++;
+                            }
+                        }
+                    }
+                }
+        }
+
+        return new PinStruct(whitePin, blackPin);
     }
 }
---
+++ src/main/java/mastermind/PinStruct.java
@@ -0,0 +1,12 @@
+package mastermind;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class PinStruct {
+    private int white;
+    private int black;
+}
