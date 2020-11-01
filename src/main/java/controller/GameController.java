57ccfeb99ca83ba22d704214b391e6b7e7d2e619
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-11-01 13:54:21 +0100

    Add New game button functionality to Mastermind game

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,15 +2,22 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import mastermind.Color;
 import mastermind.Mastermind;
 import mastermind.PinStruct;
+
+import java.io.IOException;
 
 public class GameController {
 
@@ -127,5 +134,12 @@ public class GameController {
             errorLabel.setText("Unfortunately you failed this time");
         }
 
+    }
+
+    public void processNewGame(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mastermind.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
