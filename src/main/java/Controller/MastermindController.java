4f589132d1d9e7efca8867ec715864fca4c8cc3b
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-11-10 19:48:05 +0100

    Comment Mastermind source
    If something is still ununderstandable let me know!

--- src/main/java/Controller/MastermindController.java
+++ src/main/java/Controller/MastermindController.java
@@ -102,7 +102,9 @@ public class MastermindController {
         if (lastStep > 0 && mastermind.getGameState() == 1 && rowHelper > 0) {
             errorLabel.setText("");
             ImageView view = (ImageView) leftPane.getChildren().get(lastStep - 1);
+            //Delete the last color in image view, if we can (for example we don't submit it yet)
             view.setImage(null);
+            //We need to decrease helper integers!
             lastStep--;
             rowHelper--;
         }
@@ -123,11 +125,14 @@ public class MastermindController {
      */
     public void processSubmit(ActionEvent event) {
         errorLabel.setText("");
+        //If we have 4 colors in a row we can submit our tips, otherwise we can't
         if (lastStep % 4 == 0 && lastStep != 0 && lastStep != 40) {
             PinStruct pins = mastermind.process(leftPane, lastStep);
-
+            //With this for loop, we write out pins
             for (int i = 0; i < pins.getBlack() + pins.getWhite(); i++) {
+                //This will be return the right position:
                 ImageView rightView = (ImageView) rightPane.getChildren().get(((int) Math.floor(lastStep / 4) - 1) * 4 + i);
+                //Important, we write out black pins first, after whites (not in order according to evaluation)
                 if (i < pins.getBlack()) {
                     rightView.setImage(Color.get(Color.BLACK));
                 } else
@@ -135,24 +140,25 @@ public class MastermindController {
             }
             rowHelper = 0;
 
+            //Write out in infobox if none of our tip is good
             if (pins.getBlack() + pins.getWhite() == 0) {
                 errorLabel.setText("All of your guesses are wrong!");
-            } else if (pins.getBlack() == 4) {
+            } else if (pins.getBlack() == 4) { //If all of them is good write out congratulations message in infobox and...
                 mastermind.setGameState(2);
                 errorLabel.setText("Congratulations, you win!" + "\n" + "You guessed all the colors!" + "\n" + "If you want to play another match " +
                         "\n" + "press new game button!" + "\n" + "Or if you want to play with " + "\n" + "another mini game, press " +
                         "\n" + "Back to menu button!");
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++) { //...show the secret colors!
                     setColor(i, Color.get(Color.getByValue(mastermind.getGuessColors()[i])));
                 }
             }
-        } else if(lastStep < 40 ){
+        } else if(lastStep < 40 ){ //If we have not 4 colors in a row write this error message in infobox
             errorLabel.setText("You must select 4 colors to check!" + "\n" +
                     "Use Color buttons");
-        }else {
+        }else { //If none of the previous statement, then lose this match unfortunately :( , write this message in infobox:
             mastermind.setGameState(3);
             errorLabel.setText("Unfortunately you failed this time");
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) { //Here show the secret colors like above:
                 setColor(i, Color.get(Color.getByValue(mastermind.getGuessColors()[i])));
             }
         }
