469227c8881608cb35dd970a11662522d7276763
fenyvesimate211 <fenyvesimate211@gmail.com>
2020-11-12 12:47:31 +0100

    Create 4x4 matrix and currentPlayer

--- src/main/java/Game2048/GameState.java
+++ src/main/java/Game2048/GameState.java
@@ -1,4 +1,17 @@
 package Game2048;
 
 public class GameState {
+
+    private char[][] gameState;
+    private int currentPlayer;
+
+    public GameState() {
+        this.gameState = new char[4][4];
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++) {
+                gameState[i][j] = ' ';
+            }
+        }
+        this.currentPlayer = 1;
+    }
 }
