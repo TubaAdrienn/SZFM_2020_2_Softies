7297d7d2e2366bcfaed8f28046359640e8c2b1d6
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-11-14 09:14:36 +0100

    formatting changes

--- src/main/java/Controller/SnakeController.java
+++ src/main/java/Controller/SnakeController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Helpers.PageLoader;
+import TicTacToe.GameState;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
@@ -25,25 +28,26 @@ public class SnakeController {
     private Label highScoreLabel;
 
     @FXML
-    private Button backButton;
-
-    @FXML
     private Pane grid;
 
+    private GridPane gridPane;
+    private GameState gameState;
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         generateGridPane();
+        renderSnake();
     }
 
-    private void generateGridPane(){
-        GridPane gridPane = new GridPane();
+    private void generateGridPane() {
+        gridPane = new GridPane();
         gridPane.setPrefWidth(700.5);
         gridPane.setPrefHeight(500);
         gridPane.setGridLinesVisible(true);
-        for(int i=0; i<15; i++){
-            for(int j=0; j<15; j++){
-                Label label = new Label ();
-                label.setId(createID(i,j));
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                Label label = new Label();
+                label.setId(createID(i, j));
                 label.setPrefHeight(33.0);
                 label.setPrefWidth(46.7);
                 gridPane.addRow(i, label);
@@ -53,28 +57,16 @@ public class SnakeController {
         grid.getChildren().add(gridPane);
     }
 
-    public String createID(int i, int j){
-        return String.valueOf(i)+"_"+String.valueOf(j);
+    public String createID(int i, int j) {
+        return String.valueOf(i) + "_" + String.valueOf(j);
     }
 
-    public void backToRulePage(ActionEvent event) throws IOException {
-        Button button = (Button) event.getSource();
-
-        if (button.getId().equals("backButton")) {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/rules.fxml"));
-            Parent root = fxmlLoader.load();
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        }
+    public void backToRulePage(MouseEvent event) throws IOException {
+        PageLoader.loadRules(event, "snake");
     }
 
-    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
-        for (Node node : gridPane.getChildren()) {
-            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
-                return node;
-            }
-        }
-        return null;
+    private void renderSnake() {
+        this.gameState = new GameState();
     }
+
 }
