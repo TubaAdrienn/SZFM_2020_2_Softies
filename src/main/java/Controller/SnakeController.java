a9a97e8ffaafcb19d97a9cd31991369b9997451e
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-11-14 09:09:15 +0100

    changed snake view so gridpane is generated in controller

--- src/main/java/Controller/SnakeController.java
+++ src/main/java/Controller/SnakeController.java
@@ -10,6 +10,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,8 +28,34 @@ public class SnakeController {
     private Button backButton;
 
     @FXML
-    private GridPane grid;
+    private Pane grid;
 
+    @FXML
+    public void initialize(){
+        generateGridPane();
+    }
+
+    private void generateGridPane(){
+        GridPane gridPane = new GridPane();
+        gridPane.setPrefWidth(700.5);
+        gridPane.setPrefHeight(500);
+        gridPane.setGridLinesVisible(true);
+        for(int i=0; i<15; i++){
+            for(int j=0; j<15; j++){
+                Label label = new Label ();
+                label.setId(createID(i,j));
+                label.setPrefHeight(33.0);
+                label.setPrefWidth(46.7);
+                gridPane.addRow(i, label);
+                System.out.println(label.getId());
+            }
+        }
+        grid.getChildren().add(gridPane);
+    }
+
+    public String createID(int i, int j){
+        return String.valueOf(i)+"_"+String.valueOf(j);
+    }
 
     public void backToRulePage(ActionEvent event) throws IOException {
         Button button = (Button) event.getSource();
@@ -39,5 +67,14 @@ public class SnakeController {
             stage.setScene(new Scene(root));
             stage.show();
         }
+    }
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+        return null;
     }
 }
