587acd427cb09954f3882bbbd89ff59e151dbc31
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2020-11-14 19:44:04 +0100

    almost working 2048 on console

--- src/main/java/Game2048/GameState.java
+++ src/main/java/Game2048/GameState.java
@@ -1,38 +1,57 @@
 package Game2048;
 
+import java.util.Random;
+
 public class GameState {
 
-    private char[][] gameState;
+    private int[][] gameState;
 
     public GameState() {
-        this.gameState = new char[4][4];
-        for (int i = 0; i < 4; i++){
+        this.gameState = new int[4][4];
+        for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                gameState[i][j] = '0';
+                gameState[i][j] = 0;
             }
         }
+        generateNewCells();
+        generateNewCells();
     }
 
-    public GameState(char[][] gameState, int current){
+    public GameState(int[][] gameState, int current) {
         this.gameState = gameState;
     }
 
-    public char[][] getState() {
+    public int[][] getState() {
         return this.gameState;
     }
 
-    public boolean isEmpty(){
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++) {
-                if(gameState[i][j]=='0'){
-                    return true;
-                }
-            }
-        }
-        return false;
+    public int[][] getGameState() {
+        return gameState;
     }
 
-    public void winner(){
+    public void generateNewCells() {
+        Random rand = new Random();
+        boolean success = false;
+        while (success == false) {
+            int row = rand.nextInt(4);
+            int col = rand.nextInt(4);
+            int twoOrFour = rand.nextInt(10);
+            switch (twoOrFour) {
+                case 1:
+                    twoOrFour = 4;
+                    break;
+                default:
+                    twoOrFour = 2;
+                    break;
+            }
+            if (this.gameState[row][col] == 0) {
+                this.gameState[row][col] = twoOrFour;
+                success = true;
+            }
+        }
+    }
+
+    public void winner() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 if (gameState[i][j] == 2048) {
@@ -42,24 +61,124 @@ public class GameState {
         }
     }
 
-    public void isOver(){
+    public boolean isOver() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (isEmpty()){
-                    System.out.println("Game Over");
+                if (gameState[i][j] == 0) {
+                    return false;
                 }
             }
         }
+        return true;
     }
 
     public void showState() {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.printf(String.valueOf(gameState[i][j]));
+                System.out.printf("[" + String.valueOf(gameState[i][j]) + "]");
                 if (j == 3) {
                     System.out.println();
                 }
             }
+        }
+    }
+
+    private void setCell(int i, int k, int moveI, int moveJ) {
+        int originalCell = gameState[i][k];
+        gameState[i][k] = 0;
+        gameState[moveI][moveJ] += originalCell;
+    }
+
+    public void moveCellsRight() {
+        boolean moveHappened = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 2; j >= 0; j--) {
+                for (int k = j; k < 3; k++) {
+                    if ( gameState[i][k] == gameState[i][k + 1] && gameState[i][k] != 0 ){
+                        setCell(i, k, i, k + 1);
+                        moveHappened = true;
+                    }
+                    if (gameState[i][k + 1] == 0 && gameState[i][k] != 0) {
+                        setCell(i, k, i, k + 1);
+                        moveHappened = true;
+                    }
+                }
+            }
+        }
+        if (moveHappened) generateNewCells();
+    }
+
+    public void moveCellsLeft() {
+        boolean moveHappened = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                for (int k = j; k > 0; k--) {
+                    if (gameState[i][k] == gameState[i][k - 1] && gameState[i][k] != 0 ) {
+                        setCell(i, k, i, k - 1);
+                        moveHappened = true;
+                    }
+                    if (gameState[i][k - 1] == 0 && gameState[i][k] != 0) {
+                        setCell(i, k, i, k - 1);
+                        moveHappened = true;
+                    }
+                }
+            }
+        }
+        if (moveHappened) generateNewCells();
+    }
+
+    public void moveCellsUp() {
+        boolean moveHappened = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                for (int k = j; k > 0; k--) {
+                    if (gameState[k][i] == gameState[k - 1][i] && gameState[k][i] != 0 ) {
+                        setCell(k, i, k - 1, i);
+                        moveHappened = true;
+                    }
+                    if (gameState[k - 1][i] == 0 && gameState[k][i] != 0) {
+                        setCell(k, i, k - 1, i);
+                        moveHappened = true;
+                    }
+                }
+            }
+        }
+        if (moveHappened) generateNewCells();
+    }
+
+    public void moveCellsDown() {
+        boolean moveHappened = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = j; k < 3; k++) {
+                    if (gameState[k][i] == gameState[k + 1][i] && gameState[k][i] != 0 ) {
+                        setCell(k, i, k + 1, i);
+                        moveHappened = true;
+                    }
+                    if (gameState[k + 1][i] == 0 && gameState[k][i] != 0) {
+                        setCell(k, i, k + 1, i);
+                        moveHappened = true;
+                    }
+                }
+            }
+        }
+        if (moveHappened) generateNewCells();
+    }
+
+    public void moveCells(String direction) {
+        switch (direction) {
+            case "a":
+                this.moveCellsLeft();
+                break;
+            case "s":
+                this.moveCellsDown();
+                break;
+            case "d":
+                this.moveCellsRight();
+                break;
+            case "w":
+                this.moveCellsUp();
+                break;
         }
     }
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,11 +1,23 @@
 package Main;
 
-import javafx.application.Application;
+import Game2048.GameState;
+
+import java.util.Scanner;
 
 public class Main{
 
     public static void main(String[] args) {
-        Application.launch(MyApp.class, args);
-    }
+        GameState gameState=new GameState();
+        gameState.showState();
+        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        String be;
 
-}
+        while (true){
+            be=scan.nextLine();
+            gameState.moveCells(be);
+            gameState.showState();
+            System.out.println();
+            }
+        }
+    }
