cc2575c6ecd086a0df3d986274de499671620724
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-12-04 14:14:22 +0100

    Add guessedColors JUnit test to Mastermind
    It gets random values so we can only check the length

--- src/test/java/mastermind/MastermindTest.java
+++ src/test/java/mastermind/MastermindTest.java
@@ -13,4 +13,10 @@ public class MastermindTest {
         mastermind.setGameState(2);
         assertEquals(2, mastermind.getGameState());
     }
+
+    @Test
+    void testGuessedColors() {
+        Mastermind mastermind = new Mastermind();
+        assertEquals(4, mastermind.getGuessColors().length);
+    }
 }
