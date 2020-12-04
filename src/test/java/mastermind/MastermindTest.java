16eb32f447c296cf711d5d40019cfada89d3f4b3
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-12-04 14:45:59 +0100

    Add process() JUnit test to Mastermind
    Maybe this one need some update later..
    (Not easy because use randomness)

--- src/test/java/mastermind/MastermindTest.java
+++ src/test/java/mastermind/MastermindTest.java
@@ -1,6 +1,11 @@
 package mastermind;
 
 import Mastermind.Mastermind;
+import Mastermind.PinStruct;
+import Mastermind.Color;
+import com.sun.javafx.application.PlatformImpl;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,5 +23,22 @@ public class MastermindTest {
     void testGuessedColors() {
         Mastermind mastermind = new Mastermind();
         assertEquals(4, mastermind.getGuessColors().length);
+    }
+
+    @Test
+    void testProcess() {
+        PlatformImpl.startup(() -> {});
+
+        Mastermind mastermind = new Mastermind();
+
+        GridPane leftPane = new GridPane();
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 4; j++)
+                leftPane.add(new ImageView(), i, j);
+
+        assertEquals(new PinStruct(0, 0), mastermind.process(leftPane, 5));
+
+        ((ImageView)leftPane.getChildren().get(0)).setImage(Color.get(Color.getByValue(mastermind.getGuessColors()[0])));
+        assertEquals(new PinStruct(0, 1), mastermind.process(leftPane, 5));
     }
 }
