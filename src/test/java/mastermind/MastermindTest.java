6dd1d5a1f50c99e098d4ddc2b6908db00d220519
NRaccoon <43210244+NRaccoon@users.noreply.github.com>
2020-12-04 14:05:26 +0100

    Add gameState test to Mastermind

---
+++ src/test/java/mastermind/MastermindTest.java
@@ -0,0 +1,16 @@
+package mastermind;
+
+import Mastermind.Mastermind;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MastermindTest {
+    @Test
+    void testGameState() {
+        Mastermind mastermind = new Mastermind();
+        assertEquals(1, mastermind.getGameState());
+        mastermind.setGameState(2);
+        assertEquals(2, mastermind.getGameState());
+    }
+}
